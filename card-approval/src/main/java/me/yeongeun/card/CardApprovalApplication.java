73cb8894e62ec0e5b9d5a95c96df8600879922a8
ye0ngeun <duddms2415@naver.com>
2025-09-16 13:58:14 +0900

    feat: 카드 승인계 서버 초기 세팅

---
+++ card-approval/src/main/java/me/yeongeun/card/CardApprovalApplication.java
@@ -0,0 +1,13 @@
+package me.yeongeun.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardApprovalApplication.class, args);
+	}
+
+}
---
+++ card-approval/src/test/java/me/yeongeun/card/CardApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yeongeun.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApprovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
